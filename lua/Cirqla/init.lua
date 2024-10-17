a50258e274b503a36568e1ca925afc2f0a96e566
Caesarium <timomix13@gmail.com>
2024-10-17 13:04:12 +0200

    Fix: Not loading colorscheme

--- lua/Cirqla/init.lua
+++ lua/Cirqla/init.lua
@@ -25,7 +25,7 @@ end
 -- Add command to nvim
 vim.api.nvim_create_user_command('Cirqla', function(args)
 	M.load(args)
-	vim.cmd('colorscheme cirqla')
+	vim.cmd('colorscheme Cirqla')
 end, {
 	nargs = 1,
 })
